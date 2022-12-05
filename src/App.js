b9af62da729174252afcbe74b89ae9c9efc94112
liuzhi <james_73@hotmail.com>
2022-12-05 14:39:03 -0700

    new changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css"
+import React from "react"
+import ReactDOM from "react-dom"
+
+class App extends React.componet{
+    render(){
+        return(
+            <div> hello world</div>
+        )
+    }
+}
+
+export default App
